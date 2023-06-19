0ad0378a0b5a5540876d0c265e72ac84043a0648
dutysniper <natarov.constantin@yandex.ru>
2023-06-19 21:01:45 +0300

    Initial commit

---
+++ sliderApp/sliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sliderAppApp.swift
+//  sliderApp
+//
+//  Created by Константин Натаров on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct sliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
